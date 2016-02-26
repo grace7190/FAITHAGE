ba6a0584438e5967468019d162512511ad2862b1
grace7190 <grace.zhu@mail.utoronto.ca>
2016-02-26 00:50:24 -0500

    added skill icons

--- main.py
+++ main.py
@@ -7,6 +7,10 @@ pygame.display.set_caption("#FAITHAGE")
 # Used to manage how fast the screen updates
 clock = pygame.time.Clock()
 
+# -------- Other Useful Things Here --------
+# Hide the mouse cursor
+# pygame.mouse.set_visible(False)
+
 # TESTING ONLY - global vars
 test_bg = pygame.image.load("art/bg_temp.jpg").convert() 
 test_sprite = pygame.image.load("art/pl_temp.png").convert()
@@ -20,16 +24,20 @@ skill_x = 0
 
 # Loop until the user clicks the close button.
 quit = False
-# -------- Main Program Loop -----------
+# -------- Main Program Loop ---------
 while not quit:
     # --- Main event loop
     for event in pygame.event.get(): 
         if event.type == pygame.QUIT: 
             quit = True
         elif event.type == pygame.MOUSEBUTTONDOWN:
-            print("User pressed a mouse button")
-            click_sound.play() 
-            
+            pos = pygame.mouse.get_pos()
+            if (pos[0] > skill_x and
+                pos[0] < skill_x + test_skillicon.get_width() and
+                pos[1] > 780):
+                skill_x = 0
+                click_sound.play()
+
     # --- Drawing code
     screen.blit(test_bg, (0,0))
     screen.blit(test_sprite, (600-test_sprite.get_width(),
