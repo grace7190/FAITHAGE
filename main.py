67d9817e09c0bae10c90c60d19d2f75f41b60c6a
Philip <philip.bilodeau@gmail.com>
2016-02-27 20:13:41 -0500

    Re-implemented Skill sprite which was removed by an evil teammate

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pygame
+from Skill import *
  
 pygame.init()
 screen = pygame.display.set_mode([1920,1080])
@@ -17,10 +18,12 @@ test_sprite = pygame.image.load("art/pl_temp.png").convert()
 test_sprite.set_colorkey((0,255,0))
 test_enemy = pygame.image.load("art/en_temp.png").convert()
 test_enemy.set_colorkey((0,255,0))
-test_skillicon = pygame.image.load("art/ic_temp.png").convert()
 click_sound = pygame.mixer.Sound("sound/fx_test.ogg")
-# need to make a class for skill to store the x position, destroy self?
-skill_x = 0
+click_sound.set_volume(0.1)
+
+skill = Skill()
+skill_Group = pygame.sprite.Group()
+skill_Group.add(skill)
 
 # Loop until the user clicks the close button.
 quit = False
@@ -31,12 +34,10 @@ while not quit:
         if event.type == pygame.QUIT: 
             quit = True
         elif event.type == pygame.MOUSEBUTTONDOWN:
-            pos = pygame.mouse.get_pos()
-            if (pos[0] > skill_x and
-                pos[0] < skill_x + test_skillicon.get_width() and
-                pos[1] > 780):
-                skill_x = 0
+            if pygame.mouse.get_pressed()[0]:
                 click_sound.play()
+                if skill.rect.collidepoint(pygame.mouse.get_pos()):
+                    skill.triggered = True
 
     # --- Drawing code
     screen.blit(test_bg, (0,0))
@@ -44,10 +45,10 @@ while not quit:
                               780-test_sprite.get_height()+20))
     screen.blit(test_enemy, (1320,
                              780-test_enemy.get_height()+20))
-    screen.blit(test_skillicon, (skill_x, 910)) 
-    skill_x += 5
     
-    # --- update the screen 
+    # --- update the screen
+    skill_Group.update()
+    skill_Group.draw(screen)
     pygame.display.flip()
  
     # --- Limit to 60 frames per second
